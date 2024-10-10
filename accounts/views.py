392cda10733766d14c3e16aa89d530f3072ba88e
Diego Delgado(mac) <diegoruny@gmail.com>
2024-10-09 19:46:22 -0500

    Finished project ui of login and register improved

--- accounts/views.py
+++ accounts/views.py
@@ -4,28 +4,11 @@ from django.contrib.auth import login
 
 
 # Create your views here.
-def register(request):
-    """New user registration"""
-    if request.method == 'POST':
-        form = UserCreationForm()
-    else:
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            new_user = form.save()
-            #Log the user in and redirect to product_list page
-            login(request, new_user)
-            return redirect('shop:product_list')
-    
-    #Blank/error form
-    context = {'form': form}
-    return render(request, 'registration/register.html', context)
-
 # def register(request):
-#     form = UserCreationForm()
 #     """New user registration"""
 #     if request.method == 'POST':
-        
-    
+#         form = UserCreationForm()
+#     else:
 #         form = UserCreationForm(request.POST)
 #         if form.is_valid():
 #             new_user = form.save()
@@ -36,3 +19,20 @@ def register(request):
 #     #Blank/error form
 #     context = {'form': form}
 #     return render(request, 'registration/register.html', context)
+
+def register(request):
+    form = UserCreationForm()
+    """New user registration"""
+    if request.method == 'POST':
+        
+    
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            #Log the user in and redirect to product_list page
+            login(request, new_user)
+            return redirect('shop:product_list')
+    
+    #Blank/error form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
